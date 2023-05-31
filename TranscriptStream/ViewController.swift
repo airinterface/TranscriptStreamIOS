b03cd979cf092c64f254e4ef8de03989e93c21c4
Yuri Fukuda <airinterface@gmail.com>
2023-05-31 01:14:40 -0400

    initializing draft

---
+++ TranscriptStream/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TranscriptStream
+//
+//  Created by Yuri Fukuda on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+
+    }
+
+
+}
+
